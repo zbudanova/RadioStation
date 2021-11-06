80c8a4846f0d15e3a3c901b27208466725012f57
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-11-06 11:35:51 +0500

    Updated Radio.java and add RadioTest.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,4 +1,46 @@
 package ru.netology.domain;
 
 public class Radio {
+    public int currentRadioStation;
+
+    public void setCurrentRadioStation(int currentRadioStation){
+        if (currentRadioStation < 0) {
+            return;
+        }
+        if (currentRadioStation > 9) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void NextRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 10) {
+            this.currentRadioStation = currentRadioStation +1;
+        }
+        if (currentRadioStation == 9) {
+            this.currentRadioStation = 0;
+        }
+    }
+    public void PrevRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 10) {
+            this.currentRadioStation = currentRadioStation - 1;
+        }
+        if (currentRadioStation == 0) {
+            this.currentRadioStation = 9;
+        }
+    }
+    public void setRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 0) {
+            return;
+        }
+        if (currentRadioStation < 10) {
+            return;
+        }
+
+    }
 }
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,28 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void setCurrentRadioStation() {
+    }
+
+    @Test
+    void getCurrentRadioStation() {
+    }
+
+    @Test
+    void nextRadioStation() {
+    }
+
+    @Test
+    void prevRadioStation() {
+    }
+
+    @Test
+    void setRadioStation() {
+    }
+}
